07467b851d24d2a07c06886117989f3fc0b22f43
Admiral <91305229+pooyatavan@users.noreply.github.com>
2024-09-02 21:09:34 +0330

    add converter

--- modules/image.py
+++ modules/image.py
@@ -1,9 +1,12 @@
 from PIL import Image
+import os
 
 from modules.config import Config
 from modules.tools import GetRootProject
 from modules.log import LOG
 from modules.strings import Console
+
+BASE_DIR = GetRootProject() + "\\dir"
 
 def compress_image(file_path, filename):
     try:
@@ -17,3 +20,21 @@ def compress_image(file_path, filename):
                 img.save(file_path + "\\" + "thumb_" + filename, "JPEG", quality=int(Config.read()['image']['quality']))
     except:
         LOG.warning(Console.CompressError.value)
+
+def create_thumbnail(image_path, thumb_path, size=(int(Config.read()['image']['width']), int(Config.read()['image']['height']))):
+    with Image.open(image_path) as img:
+        img.thumbnail(size)
+        img.save(thumb_path)
+
+def Scan():
+    counter = 0
+    for root, dirs, files in os.walk(BASE_DIR):
+        for file_name in files:
+            if "thumb_" not in file_name:
+                if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff')):
+                    image_path = os.path.join(root, file_name)
+                    thumb_path = os.path.join(root, os.path.splitext('thumb_' + file_name)[0] + os.path.splitext(file_name)[1])
+                    if not os.path.exists(thumb_path):
+                        counter = counter + 1
+                        create_thumbnail(image_path, thumb_path)      
+    LOG.info(Console.ScanForThumbnail.value.format(counter=counter))
