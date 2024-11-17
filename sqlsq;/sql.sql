d7d8999ed7b271daeeff8e893630da4277c36985
Admiral <91305229+pooyatavan@users.noreply.github.com>
2024-11-17 21:18:25 +0330

    Add files via upload

--- sqlsq;/sql.sql
+++ sqlsq;/sql.sql
@@ -1,1 +1,61 @@
+/*
+SQLyog Professional v13.1.1 (64 bit)
+MySQL - 8.0.40 : Database - araax
+*********************************************************************
+*/
 
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`araax` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `araax`;
+
+/*Table structure for table `logs` */
+
+DROP TABLE IF EXISTS `logs`;
+
+CREATE TABLE `logs` (
+  `number` bigint DEFAULT NULL,
+  `date` varchar(200) DEFAULT NULL,
+  `user` varchar(100) DEFAULT NULL,
+  `operate` varchar(100) DEFAULT NULL,
+  `detail` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `perm` */
+
+DROP TABLE IF EXISTS `perm`;
+
+CREATE TABLE `perm` (
+  `id` bigint DEFAULT NULL,
+  `delete` int DEFAULT NULL,
+  `upload` int DEFAULT NULL,
+  `newfolder` int DEFAULT NULL,
+  `rename` int DEFAULT NULL,
+  `log` int DEFAULT NULL,
+  `admin` int DEFAULT NULL,
+  `print` int DEFAULT NULL,
+  `search` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint NOT NULL,
+  `username` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
