febbac9ddd775238c79b44c665a05eda400ab74c
Admiral <91305229+pooyatavan@users.noreply.github.com>
2025-01-24 09:20:43 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging, eventlet, sys, os
-from eventlet import wsgi
+#from eventlet import wsgi
+from waitress import serve
 
 # no cache files for python
 sys.dont_write_bytecode = True
@@ -10,17 +11,16 @@ from modules.strings import Console
 from modules.tools import network
 from modules.flask import app, FlaskAPP
 from modules.thread import thread
-from modules.image import Scan
 
 if __name__ == '__main__':
     try:
         UPLOAD_FOLDER = os.path.join(os.getcwd(), 'dir')
         if not os.path.exists(UPLOAD_FOLDER):
             LOG.error(Console.DirFolder.value)
-            #os.makedirs(UPLOAD_FOLDER)
+            os.makedirs(UPLOAD_FOLDER)
+            LOG.info(Console.MakeDirFolder.value)
         else:
-            thread(Scan())
-            FlaskAPP()
+            thread(FlaskAPP())
             LOG.info(Console.NetworkInfo.value.format(ip=network()))
             LOG.info(Console.ServerRunning.value.format(ip=network(), port=80))
     except:
@@ -29,4 +29,5 @@ if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     logging.getLogger("waitress.queue").setLevel(logging.ERROR)
-    wsgi.server(eventlet.listen((network(), int(Config.read()['core']['port']))), app, log_output=False)
+    serve(app, host=network(), port=int(Config.read()['core']['port']))
+    #wsgi.server(eventlet.listen((network(), int(Config.read()['core']['port']))), app, log_output=False)
