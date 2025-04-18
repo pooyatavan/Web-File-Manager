5b46031dd9c8786b096397c31435f2a9cdbc3aea
Admiral <91305229+pooyatavan@users.noreply.github.com>
2025-04-19 01:29:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging, eventlet, sys, os
-#from eventlet import wsgi
-from waitress import serve
+import logging, sys, os, eventlet
+from eventlet import wsgi
 
 # no cache files for python
 sys.dont_write_bytecode = True
@@ -10,7 +9,9 @@ from modules.config import Config
 from modules.strings import Console
 from modules.tools import network
 from modules.flask import app, FlaskAPP
-from modules.thread import thread
+from modules.tools import Silence
+
+BASE_DIR = app.config['UPLOAD_FOLDER']
 
 if __name__ == '__main__':
     try:
@@ -20,14 +21,13 @@ if __name__ == '__main__':
             os.makedirs(UPLOAD_FOLDER)
             LOG.info(Console.MakeDirFolder.value)
         else:
-            thread(FlaskAPP())
+            FlaskAPP()
             LOG.info(Console.NetworkInfo.value.format(ip=network()))
             LOG.info(Console.ServerRunning.value.format(ip=network(), port=80))
+            app.jinja_env.auto_reload = True
+            app.config['TEMPLATES_AUTO_RELOAD'] = True
+            logging.getLogger("waitress.queue").setLevel(logging.ERROR)
+            eventlet.spawn(Silence)
+            wsgi.server(eventlet.listen((network(), int(Config.read()['core']['port']))), app, log_output=False)
     except:
         LOG.error(Console.ServerError.value)
-
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    logging.getLogger("waitress.queue").setLevel(logging.ERROR)
-    serve(app, host=network(), port=int(Config.read()['core']['port']))
-    #wsgi.server(eventlet.listen((network(), int(Config.read()['core']['port']))), app, log_output=False)
